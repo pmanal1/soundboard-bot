f1951b62039374801ec9e9120de30730018ffdf7
Paulo Manalo <paulomanalo792@gmail.com>
2026-03-01 20:42:14 -0500

    Changes application command route

--- soundboard-bot.ts
+++ soundboard-bot.ts
@@ -148,7 +148,7 @@ const rest = new REST({ version: "10"}).setToken(process.env.DISCORD_TOKEN!);
 
 (async () => {
   await rest.put(
-    Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!),
+    Routes.applicationCommands(process.env.CLIENT_ID!),
     { body: commands }
   );
 
